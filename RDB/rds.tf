8218e176670c2dd6361c24ff29ccbf57bb57ebcd
Hayden <62683492+randine@users.noreply.github.com>
2020-10-06 13:26:30 +1100

    Add files via upload

---
+++ RDB/rds.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+provider "aws" {
+  version = ">= 2.28.1"
+  region  = "ap-southeast-2"
+}
+
+resource "aws_subnet" "main-new" {
+  vpc_id     = "vpc-0d13470c3cf93e14d"
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "thisisatest"
+  parameter_group_name = "default.mysql5.7"
+  db_subnet_group_name = "vpc-0d13470c3cf93e14d"
+}
+
